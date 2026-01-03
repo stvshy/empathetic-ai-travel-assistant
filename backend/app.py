7fc03d7e9c314e0d6170430c16a9a87348b96d91
WeNeedToCodeDeeper <rafal.trzak@gmail.com>
2026-01-03 21:49:23 +0100

    Translation + emotions
    Backend now will translate and detect emotions and send it in response

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,11 @@ from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from flask_cors import CORS
 import logging
+import whisper
+import os
+import tempfile
+from transformers import pipeline
+import subprocess
 
 
 logging.basicConfig()
@@ -12,6 +17,10 @@ load_dotenv()
 app = Flask(__name__)
 
 CORS(app, supports_credentials=True)
+
+
+model = whisper.load_model("turbo")
+classifier = pipeline("audio-classification",model="superb/wav2vec2-base-superb-er")
 
 
 @app.route("/process", methods=["POST"])
@@ -31,9 +40,49 @@ def process():
     size = audio_file.tell()
     audio_file.seek(0)     # Reset file pointer to start
 
+    translation_output = None
+    emotion_output = None
+
     if size > 0:
-        logger.error("it works")  # Your requested log message
-        return jsonify({"message": "Audio received successfully"}), 200
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".webm") as webm_file:
+            audio_file.save(webm_file.name)
+            webm_path = webm_file.name
+        try:
+            result = model.transcribe(webm_path,language="polish")
+
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as wav:
+                wav_path = wav.name
+
+            try:
+                subprocess.run(
+                    [
+                        "ffmpeg",
+                        "-y",
+                        "-i", webm_path,
+                        "-ac", "1",
+                        "-ar", "16000",
+                        wav_path
+                    ],
+                    check=True,
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL
+                )
+
+                outputs = classifier(wav_path, top_k=None)
+                logging.error(outputs)
+                emotion_output = outputs
+
+            finally:
+                os.remove(wav_path)
+
+            logging.error(result["text"])
+            translation_output = result["text"]
+            return jsonify({"translation": translation_output, "emotions":emotion_output}), 200
+        except Exception as e:
+            logger.exception("AI failed")
+            return jsonify({"error": "Transcription failed"}), 500
+        finally:
+            os.remove(webm_path)
     else:
         logger.error("Received audio file is empty")
         return jsonify({"error": "Audio file is empty"}), 400
